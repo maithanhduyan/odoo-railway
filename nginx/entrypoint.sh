cd3cdbfe6a348a2f48162df5d3daf0f9a82a8cb6
Mai Thành Duy An <tiachop0102@gmail.com>
2026-03-15 01:13:32 +0900

    Update Odoo database name and enhance Nginx configuration for DNS resolution in entrypoint script

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-envsubst '$PORT $ODOO_HOST $ODOO_PORT $ODOO_WS_PORT' \
-  < /etc/nginx/templates/odoo.conf.template \
+# Extract DNS resolver from system for runtime domain resolution
+export DNS_RESOLVER=$(grep -m1 '^nameserver' /etc/resolv.conf | awk '{print $2}')
+: "${DNS_RESOLVER:=8.8.8.8}"
+
+envsubst '$PORT $ODOO_HOST $ODOO_PORT $ODOO_WS_PORT $DNS_RESOLVER' \
+  < /etc/nginx/odoo.conf.template \
   > /etc/nginx/conf.d/odoo.conf
 
 exec nginx -g 'daemon off;' 2>&1
